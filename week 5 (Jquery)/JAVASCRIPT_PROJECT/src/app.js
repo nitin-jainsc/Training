a51d31886516553334196f39be5d07f092433145
Nitin Jain <nitin.jain@quovantis.com>
2018-03-03 02:54:04 +0530

    initial save state of dashboard added

--- week 5 (Jquery)/JAVASCRIPT_PROJECT/src/app.js
+++ week 5 (Jquery)/JAVASCRIPT_PROJECT/src/app.js
@@ -1,8 +1,9 @@
 window.$ = window.jQuery = require('jquery') // required for bootstrap
 window.Popper = require('popper.js') // required for tooltip, popup...
 require('bootstrap')
-require('chart.js')
-import { calculateCurrency } from "./currencyCalculator";
+import {
+    calculateCurrency
+} from "./currencyCalculator";
 import {
     activeViewFetcher
 } from "./dasboardSelector";
@@ -11,11 +12,9 @@ import {
     fetchCurrency
 } from "./currencySelector";
 
-import './index.scss' // include bootstrap css file with own modifications
-import {
-    dataFetchApi
-} from './fetchApi';
+import './index.scss'
 
+// include bootstrap css file with own modifications
 // tooltip and popover require javascript side modification to enable them (new in Bootstrap 4)
 // use tooltip and popover components everywhere
 $(function () {
@@ -24,20 +23,39 @@ $(function () {
 })
 
 // Your code here....
+
+
+// This function is used to fetch and load the current view of the dashboard 
+// based on the sidebar cick.
 let viewAnchors = document.getElementById("dashboardSidebar").querySelectorAll(":scope > li > a")
 for (let index = 0; index < viewAnchors.length; index++) {
+    //viewAnchors is the nodelist of all the anchor tags in navbar
     const element = viewAnchors[index];
     element.addEventListener("click", () => {
+        // Onclik event listener is added on anchor tag which fetches and loads the current view on click on the specific anchor tag
         activeViewFetcher.fetchView(element);
     })
 }
 
-$("#getResult").on('click',()=>{
-    console.log($("#fromVal").val());
-    
-    if($("#fromVal").val()==""){
-       alert("No value present. Plz enter some value first")
-    }else{
+// Code to save and restore te page state in session Storage
+$(window).on("load", () => {
+    // Check for new window to load homepage
+    if (sessionStorage.getItem("sectionId") == undefined) {
+        activeViewFetcher.updateView("section1");
+    } else {
+        // Check for refresh or reload from saved state
+        activeViewFetcher.updateView(sessionStorage.getItem("sectionId"))
+    }
+})
+
+// This function is used to calculate the result of the currency converter
+// getResult is the submit button for currency converter
+$("#getResult").on('click', () => {
+    // Verification to check for the empty values in the fromVal input box  
+    if ($("#fromVal").val() == "") {
+        alert("No value present. Plz enter some value first")
+    } else {
+        // User defined function to convert the currency based on the current exchange rates
         calculateCurrency.calculateExchangeValue();
     }
 });
--- week 5 (Jquery)/JAVASCRIPT_PROJECT/src/dasboardSelector.js
+++ week 5 (Jquery)/JAVASCRIPT_PROJECT/src/dasboardSelector.js
@@ -1,23 +1,44 @@
+require("jquery")
+
 import {
     activeViewLoader
 } from "./viewLoader"
 
+// Module to manage view of the dashboard
 export const activeViewFetcher = (() => {
+
+    // Private function to load the view functionality
     let loadView = (anchor) => {
         activeViewLoader.loadView(anchor);
 
     }
     return {
+        // Closure function to activate(show) clicked view 
         fetchView: (anchor) => {
+            // viewPane is the id of the section to be activated
             const viewPane = anchor.href.substring(23, anchor.href.length);
             const mainSection = document.getElementById('viewPoint').querySelectorAll(":scope > div")
             for (let i = 0; i < mainSection.length; i++) {
                 if (mainSection[i].getAttribute("id") == viewPane) {
+                    // Adds active view to the session of the window for state save and refresh
+                    sessionStorage.setItem("sectionId", viewPane);
                     mainSection[i].classList.remove("hide");
                     loadView(viewPane);
                 } else {
                     mainSection[i].classList.add("hide");
                 }
+            }
+        },
+        // Closure function to update the window from the saved state
+        updateView: (anchor) => {
+            const mainSection = $("#viewPoint").children();    
+            for (let section of mainSection) {
+                if ($(section).attr("id") == anchor) {
+                    $(section).removeClass("hide");
+                    loadView(anchor);
+                } else {
+                    $(section).addClass("hide");
+                }                
             }
         }
     }
